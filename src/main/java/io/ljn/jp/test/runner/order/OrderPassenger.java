ec13627bef5eaf67996c5c8c033f4a0cf818832e
Linus Norton <linusnorton@gmail.com>
2020-05-13 14:20:26 +0100

    fixed an issue with the order response

--- src/main/java/io/ljn/jp/test/runner/order/OrderPassenger.java
+++ src/main/java/io/ljn/jp/test/runner/order/OrderPassenger.java
@@ -1,5 +1,7 @@
 package io.ljn.jp.test.runner.order;
 
+import java.util.List;
+
 public class OrderPassenger {
     public final String passengerId = null;
     public final String channelPassengerId = "1";
@@ -7,5 +9,5 @@ public class OrderPassenger {
     public final String gender = null;
     public final int adultFlag = 1;
     public final String railCardId = null;
-    public final String payloadIds = null;
+    public final List<String> payloadIds = null;
 }
